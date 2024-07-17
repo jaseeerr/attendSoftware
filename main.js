4c4d5ad5f9d4b037815cf9f25e956af3b5c96b53
jaseeerr <jaseerhasbulla01@gmail.com>
2024-07-17 19:00:27 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    icon: path.join(__dirname, 'logoFinal1.png'),
+  });
+
+  mainWindow.loadURL('https://attend.honordubai.com');
+
+  // Remove the default menu bar
+  Menu.setApplicationMenu(null);
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
